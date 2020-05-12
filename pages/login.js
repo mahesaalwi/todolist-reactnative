4789b4b6e151cf41b698913af07822057c085aa3
MahesaAlwi <43037291+mahesaalwi@users.noreply.github.com>
2020-05-12 18:39:26 +0700

    Update Login chjange function class

--- pages/login.js
+++ pages/login.js
@@ -1,97 +1,52 @@
 import React from 'react';
 import {View, StyleSheet, Image} from 'react-native';
 import Logos from '../images/logo.png';
-import firebase from 'firebase';
 
 import {Form, Item, Input, Button, Text} from 'native-base';
 
-class login extends React.Component {
-  state = {email: '', password: '', errorMessage: '', loading: false};
+const login = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textheader}>
+        Hello <Text style={styles.txtusers}>Users</Text>
+      </Text>
+      <Text style={styles.textbottom}>Welcome Back</Text>
+      <Text style={styles.textname}>Please sign in to continue</Text>
 
-  handleLogin = () => {
-    console.log('handleLogin');
-  };
+      <Image style={styles.tinyLogo} source={Logos} />
 
-  onBottomPress = () => {
-    const {email, password} = this.state;
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email.trim(), password)
-      .then(this.onLoginSuccess.bind(this));
+      <Form style={styles.forminput}>
+        <Item regular>
+          <Input
+            style={styles.txtinput}
+            placeholder="Nama pengguna username atau email"
+          />
+        </Item>
+        <Item regular style={styles.pwd}>
+          <Input
+            secureTextEntry={true}
+            style={styles.txtinput}
+            placeholder="Kata Sandi"
+          />
+        </Item>
+        <Button
+          style={styles.btnlogin}
+          onPress={() => navigation.navigate('Homes')}>
+          <Text style={styles.text}>Masuk</Text>
+        </Button>
+      </Form>
 
-    if (email == '') {
-      alert('Please fill username !');
-      return false;
-    } else if (password == '') {
-      alert('Please fill Password !');
-      return false;
-    }
-    return true;
-  };
-  /* field validation login */
-
-  making_api_call = () => {
-    if (this.onBottomPress()) {
-      alert('Successfuly Login!');
-    }
-  };
-
-  onLoginSuccess = () => {
-    this.setState({
-      error: '',
-      loading: false,
-    });
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.textheader}>
-          Hello <Text style={styles.txtusers}>Users</Text>
+      <Text style={styles.Signup}>
+        Belum punya akun?{' '}
+        <Text
+          style={styles.txtsecond}
+          onPress={() => navigation.navigate('SignUp')}>
+          buat disini
         </Text>
-        <Text style={styles.textbottom}>Welcome Back</Text>
-        <Text style={styles.textname}>Please sign in to continue</Text>
-
-        <Image style={styles.tinyLogo} source={Logos} />
-
-        <Form style={styles.forminput}>
-          <Item regular>
-            <Input
-              style={styles.txtinput}
-              placeholder="Nama pengguna username atau email"
-              onChangeText={email => this.setState({email: email})}
-              value={this.state.email}
-            />
-          </Item>
-          <Item regular style={styles.pwd}>
-            <Input
-              secureTextEntry={true}
-              style={styles.txtinput}
-              placeholder="Kata Sandi"
-              onChangeText={password => this.setState({password: password})}
-              value={this.state.password}
-            />
-          </Item>
-          <Button
-            style={styles.btnlogin}
-            onPress={() => this.props.navigation.navigate('Homes')}>
-            <Text style={styles.text}>Masuk</Text>
-          </Button>
-        </Form>
-        <Text style={styles.errorMessage}>{this.state.errorMessage}</Text>
-
-        <Text style={styles.Signup}>
-          Belum punya akun?{' '}
-          <Text
-            style={styles.txtsecond}
-            onPress={() => this.props.navigation.navigate('SignUp')}>
-            buat disini
-          </Text>
-        </Text>
-      </View>
-    );
-  }
-}
+      </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   forminput: {
